f9c78d66b863742119adfec143a3c67e92b7de67
tanaka-neo <n.tanaka@a-force.co.jp>
2026-05-27 17:29:05 +0900

    テスト回答の追加

--- src/main/java/jp/co/aforce/servlet/NewTweetServlet.java
+++ src/main/java/jp/co/aforce/servlet/NewTweetServlet.java
@@ -1,12 +1,15 @@
 package jp.co.aforce.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import jp.co.aforce.dao.TweetDAO;
 
 @WebServlet("/new_tweet")
@@ -15,13 +18,17 @@ public class NewTweetServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		PrintWriter out=response.getWriter();
 		String content = request.getParameter("content");
 		String author = request.getParameter("author");
+		
+		HttpSession session=request.getSession();
 		
 		TweetDAO tweetDAO = new TweetDAO();
 		try {
 			tweetDAO.addTweet(content, author);
 		} catch (Exception e) {
+			out.println("ツイート失敗しました");
 			e.printStackTrace();
 		}
 		
--- src/main/webapp/tweet_list.jsp
+++ src/main/webapp/tweet_list.jsp
@@ -8,6 +8,7 @@
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
+	<%@page import="jp.co.aforce.beans.Tweet, java.util.List" %>
 	<div class="container">
 		<h1>ツイート一覧</h1>
 		<%-- 新規投稿 --%>
@@ -16,14 +17,24 @@
 		</p>
 
 		<%-- ツイート一覧の表示 --%>
+		
 		<ul class="tweet-list">
+<% List<Tweet> tweets=(List<Tweet>)request.getAttribute(tweets); %>
+			
 			<li>
+			<%for (Tweet t : list){ %>	
 				<div class="tweet-content">
-					<p>content（ツイート内容）</p>
-					<p class="tweet-info">投稿者: author - 投稿日時: posted_at</p>
+					<%=t.getContent() %><br>
+					<a class="tweet-info">
+						<%=t.getAuthor() %>
+						<%=t.getPostedAt() %>
+					</a>
 				</div>
+					<%} %>
 			</li>
+		
 		</ul>
+	
 	</div>
 </body>
 </html>
